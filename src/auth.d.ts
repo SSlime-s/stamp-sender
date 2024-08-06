48f48c1f5f58864429664d8f19b0c28b55691a69
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2024-08-06 20:49:07 +0900

    💚 biome いれる

--- src/auth.d.ts
+++ src/auth.d.ts
@@ -1,9 +1,9 @@
 import type { DefaultSession } from "next-auth";
 
 module "next-auth" {
-  export interface Session extends DefaultSession {
-    user: {
-      accessToken: string;
-    } & DefaultSession["user"];
-  }
+	export interface Session extends DefaultSession {
+		user: {
+			accessToken: string;
+		} & DefaultSession["user"];
+	}
 }
